2150b22bab400ea1bcbfcfcd0ffcf0028eaa2b54
Cheryl <cheryljfowlie@me.com>
2020-12-14 21:15:56 +1100

    Fix models

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const Workout = require("../models/index.js");
+const Workout = require("../models");
 
 
 module.exports = function (app) {
