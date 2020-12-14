fca421f58005aa834e54d6808a9d93c668cedc31
Cheryl <cheryljfowlie@me.com>
2020-12-14 21:05:23 +1100

    Create workout schema and api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var router = express.Router();
+const Workout = require("../models/index.js");
+
+
+module.exports = function (app) {
+
+    router.use(function timeLog(req, res, next) {
+        console.log("Time: ", Date.now());
+        next();
+    });
+
+    router.get("/api/workouts", (req, res) => {
+        Workout.find()
+            .then(workouts => {
+                res.json(workouts);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    router.post("/api/workouts", (req, res) => {
+        Workout.create({})
+            .then(workouts => {
+                res.json(workouts);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    router.put("/api/workouts/:id", ({ body, params }, res) => {
+        Workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+
+            { new: true, runValidators: true }
+        )
+            .then(workouts => {
+                res.json(workouts);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    router.get("/api/workouts/range", (req, res) => {
+        Workout.find({})
+            .then(workouts => {
+                // console.log(workouts)
+                res.json(workouts);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+};
